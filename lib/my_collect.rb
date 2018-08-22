b31d65c0a428f28a7a3a1903bd61cbf3736320d8
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-22 21:58:59 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  a = []
+  count = 0 
+  while count < array.length do 
+    if yield(array[count]) == true 
+      a << array[count]
+    end 
+    count += 1 
+  end 
+  a 
+end 
